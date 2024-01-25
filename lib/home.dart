37b7c5faade6be965c59c2b9135507d321de76bf
MoonLv <rievaldafellisa@gmail.com>
2024-01-26 02:00:11 +0700

    add read button

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -16,7 +17,19 @@ class Home extends StatelessWidget {
             children: [
               OutlinedButton(onPressed: () {}, child: const Text('C')),
               const SizedBox(height: 20),
-              OutlinedButton(onPressed: () {}, child: const Text('R')),
+              OutlinedButton(
+                onPressed: () async {
+                  final result = await FirebaseFirestore.instance.collection('username').get();
+                  debugPrint(result.toString());
+                  debugPrint(result.docs.toString());
+                  debugPrint(result.docs[0].toString());
+                  debugPrint(result.docs[0].id.toString());
+                  debugPrint(result.docs[0].data().toString());
+                  debugPrint(result.docs[0].data()['nama'].toString());
+                  debugPrint(result.docs[0].data()['umur'].toString());
+                },
+                child: const Text('R'),
+              ),
               const SizedBox(height: 20),
               OutlinedButton(onPressed: () {}, child: const Text('U')),
               const SizedBox(height: 20),
