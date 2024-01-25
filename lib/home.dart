1ece600ddd8a053fb0edccf976bcd1b4483908c6
MoonLv <rievaldafellisa@gmail.com>
2024-01-26 02:50:56 +0700

    add update & delete button

--- lib/home.dart
+++ lib/home.dart
@@ -16,24 +16,50 @@ class Home extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               OutlinedButton(
-                  onPressed: () async {
-                    await FirebaseFirestore.instance.collection('username').doc('aaa').set({
-                      'nama': 'coco',
-                      'umur': 12,
-                    });
-                  },
-                  child: const Text('C')),
+                onPressed: () async {
+                  await FirebaseFirestore.instance.collection('username').doc('aaa').set({
+                    'nama': 'coco',
+                    'umur': 12,
+                  });
+                },
+                child: const Text('C'),
+              ),
               const SizedBox(height: 20),
               OutlinedButton(
                 onPressed: () async {
-                  await FirebaseFirestore.instance.collection('username').get();
+                  final collectionResult = await FirebaseFirestore.instance.collection('username').get();
+                  debugPrint(collectionResult.docs.toString());
                 },
-                child: const Text('R'),
+                child: const Text('R Collection'),
               ),
               const SizedBox(height: 20),
-              OutlinedButton(onPressed: () {}, child: const Text('U')),
+              OutlinedButton(
+                onPressed: () async {
+                  final documentResult = await FirebaseFirestore.instance.collection('username').doc('aaa').get();
+                  debugPrint(documentResult.data().toString());
+                  debugPrint(documentResult.data()!['nama'].toString());
+                  debugPrint(documentResult.data()!['umur'].toString());
+                  debugPrint(documentResult.id.toString());
+                },
+                child: const Text('R Document'),
+              ),
               const SizedBox(height: 20),
-              OutlinedButton(onPressed: () {}, child: const Text('D')),
+              OutlinedButton(
+                onPressed: () async {
+                  await FirebaseFirestore.instance.collection('username').doc('aaa').update({
+                    'nama': 'cucu',
+                    'umur': 31,
+                  });
+                },
+                child: const Text('U'),
+              ),
+              const SizedBox(height: 20),
+              OutlinedButton(
+                onPressed: () async {
+                  await FirebaseFirestore.instance.collection('username').doc('aaa').delete();
+                },
+                child: const Text('D'),
+              ),
               const SizedBox(height: 20),
             ],
           ),
